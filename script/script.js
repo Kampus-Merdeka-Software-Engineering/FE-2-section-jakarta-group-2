29a53d832bf19f9779c8d5aaa8e48599b30e2bca
Bagus Adianto <bagusadianto5@gmail.com>
2023-12-12 12:34:58 +0700

    Update FE

--- script/script.js
+++ script/script.js
@@ -41,6 +41,94 @@ document.addEventListener('DOMContentLoaded', function () {
 
 /* Carousel Script */
 
+/* Data Liburan card */
+// Ambil data dari API
+fetch('http://localhost:3000/tempat-liburan') 
+.then(response => response.json())
+.then(data => {
+  const cardsContainer = document.getElementById('cards-container-holidayPlace');
+
+  data.forEach(place => {
+    const card = document.createElement('div');
+    card.classList.add('carousel');
+
+    const imageURL = `http://localhost:3000/images/${place.image}`;
+
+    card.innerHTML = `
+      <div class="card">
+          <div class="containerImageCard">
+              <img src="${imageURL}" alt="${place.location_name}" width="300px" height="220px">
+              <div class="containerTextCardImage">
+                  <div class="">
+                      ${place.date}
+                  </div>
+              </div>
+          </div>
+          <div class="containerDescCard">
+              <a href="#" class="titleCard">
+                  ${place.location_name}
+              </a>
+              <div class="descCard">
+                  ${place.description}
+              </div>
+              <div class="footerCard">
+                  Rp. ${place.price}
+              </div>
+          </div>
+      </div>
+    `;
+    
+    cardsContainer.appendChild(card);
+  });
+})
+.catch(error => {
+  console.error('Terjadi kesalahan:', error);
+});
+
+/* Data Liburan card */
+// Ambil data dari API
+fetch('http://localhost:3000/penginapan') 
+.then(response => response.json())
+.then(data => {
+  const cardsContainer = document.getElementById('cards-container-lodgings');
+
+  data.forEach(place => {
+    const card = document.createElement('div');
+    card.classList.add('carousel');
+
+    const imageURL = `http://localhost:3000/images/${place.image}`;
+
+    card.innerHTML = `
+      <div class="card">
+          <div class="containerImageCard">
+              <img src="${imageURL}" alt="${place.location_name}" width="300px" height="220px">
+              <div class="containerTextCardImage">
+                  <div class="">
+                      ${place.date}
+                  </div>
+              </div>
+          </div>
+          <div class="containerDescCard">
+              <a href="#" class="titleCard">
+                  ${place.location_name}
+              </a>
+              <div class="descCard">
+                  ${place.description}
+              </div>
+              <div class="footerCard">
+                  Rp. ${place.price}
+              </div>
+          </div>
+      </div>
+    `;
+    
+    cardsContainer.appendChild(card);
+  });
+})
+.catch(error => {
+  console.error('Terjadi kesalahan:', error);
+});
+
 
 
 
